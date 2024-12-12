8a8303a435f8330cdbff9cb02cf1f83411334d1b
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2024-12-11 21:12:48 -0500

    uncomment gitognore

---
+++ zcompress.sh
@@ -0,0 +1,3 @@
+# qsz/bin/sz3 -f -i vx.f32 -z vx.f32.sz -1 280953867 -M ABS 1e-3 -c sz3.config
+qsz/bin/sz3 -f -i vx_25.f32 -z vx_25.f32.sz -1 280953864 -M ABS 1e-3 -c sz3.config
+# qsz/bin/sz3 -f -i vx.f32 -z vx.f32.sz -1 280953867 -M ABS 1e-3 
